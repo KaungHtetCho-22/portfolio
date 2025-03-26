d89639e43c306cc182a269f02cfb45e941cb34cc
KaungHtetCho-22 <kaunghtetcho.ait@gmail.com>
2025-03-26 16:49:26 +0700

    initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 const sections = ['themeToggleContainer', 'hero', 'about', 'skills', 'links', 'projects'];
 
 sections.forEach(section => {
-  fetch(`components/${section}.html`)
+  fetch(`/portfolio/components/${section}.html`)
     .then(res => {
       if (!res.ok) {
         throw new Error(`Failed to fetch ${section}: ${res.status}`);
